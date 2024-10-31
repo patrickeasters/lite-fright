156e74a5c21a3f096578075ede644d2bd4e00194
Patrick Easters <patrick@patrickeasters.com>
2024-10-30 22:25:31 -0400

    Add wifi reconnect logic

--- src/main.ino
+++ src/main.ino
@@ -50,6 +50,24 @@ void connectToWiFi() {
   Serial.println("Connected to WiFi");
 }
 
+void WiFiStationConnected(WiFiEvent_t event, WiFiEventInfo_t info){
+  Serial.println("Connected to AP successfully!");
+}
+
+void WiFiGotIP(WiFiEvent_t event, WiFiEventInfo_t info){
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void WiFiStationDisconnected(WiFiEvent_t event, WiFiEventInfo_t info){
+  Serial.println("Disconnected from WiFi access point");
+  Serial.print("WiFi lost connection. Reason: ");
+  Serial.println(info.wifi_sta_disconnected.reason);
+  Serial.println("Trying to Reconnect");
+  WiFi.begin(ssid, password);
+}
+
 void reconnect() {
   // Loop until we're reconnected
   while (!client.connected()) {
